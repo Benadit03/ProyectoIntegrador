a10253fa170d40fa81b6cd90c99687ea42ae48f3
Benadit03 <94730340+Benadit03@users.noreply.github.com>
2022-05-01 01:34:10 -0300

    Creación del Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoApComponent } from './component/logo-ap/logo-ap.component';
+import { HeaderComponent } from './component/header/header.component';
+import { SocialComponent } from './component/social/social.component';
+import { ButtonLoginComponent } from './component/button-login/button-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoApComponent,
+    HeaderComponent,
+    SocialComponent,
+    ButtonLoginComponent
   ],
   imports: [
     BrowserModule,
