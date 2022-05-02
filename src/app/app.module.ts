aa39fb44a581e2f4a3bc22d1e31765a7d18310e6
Benadit03 <94730340+Benadit03@users.noreply.github.com>
2022-05-02 04:51:57 -0300

    Creación del about-me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogoApComponent } from './component/logo-ap/logo-ap.component';
 import { HeaderComponent } from './component/header/header.component';
 import { SocialComponent } from './component/social/social.component';
 import { ButtonLoginComponent } from './component/button-login/button-login.component';
+import { BannerComponent } from './component/banner/banner.component';
+import { AboutMeComponent } from './component/about-me/about-me.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { ButtonLoginComponent } from './component/button-login/button-login.comp
     LogoApComponent,
     HeaderComponent,
     SocialComponent,
-    ButtonLoginComponent
+    ButtonLoginComponent,
+    BannerComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
