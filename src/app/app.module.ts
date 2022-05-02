ef776f73716ab20c83070c451449869adc2d3a54
Benadit03 <94730340+Benadit03@users.noreply.github.com>
2022-05-02 06:11:42 -0300

    Optimizar Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LogoApComponent } from './component/logo-ap/logo-ap.component';
-import { HeaderComponent } from './component/header/header.component';
+import { HeaderComponent } from "./component/header/HeaderComponent";
 import { SocialComponent } from './component/social/social.component';
-import { ButtonLoginComponent } from './component/button-login/button-login.component';
-import { BannerComponent } from './component/banner/banner.component';
 import { AboutMeComponent } from './component/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogoApComponent,
     HeaderComponent,
     SocialComponent,
-    ButtonLoginComponent,
-    BannerComponent,
     AboutMeComponent
   ],
   imports: [
